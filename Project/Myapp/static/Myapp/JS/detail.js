702c6d0dbc61f60f7cb2e97074fa094d412a2792
Shibu Shrestha <shibushrstha12@gmail.com>
2023-04-10 22:08:15 +0545

    removed all the global scope

--- Project/Myapp/static/Myapp/JS/detail.js
+++ Project/Myapp/static/Myapp/JS/detail.js
@@ -1,35 +1,31 @@
-const numberInput = document.getElementById('orderQty');
-numberInput.style['-webkit-appearance'] = 'textfield';
-numberInput.style['-moz-appearance'] = 'textfield';
-
-
-let quantity = document.getElementById('orderQty').value;
-console.log(quantity);
+const qtyInput = document.getElementById('orderQty');
+qtyInput.style['-webkit-appearance'] = 'textfield';
+qtyInput.style['-moz-appearance'] = 'textfield';
 
 document.getElementById('subQty').style.cursor = 'pointer';
 function addQty(){
+    let quantity = qtyInput.value;
     if(quantity < 5){
         document.getElementById('subQty').style.cursor = 'pointer';
         quantity++;
         console.log(quantity);
-        document.getElementById('orderQty').value = quantity;
-    }else if(quantity === 5){
+        qtyInput.value = quantity;
+    }else{
         document.getElementById('addQty').style.cursor = 'no-drop';
         console.log('You connot order more than 5 items.')
-        return;
     }
 };
 
 document.getElementById('addQty').style.cursor = 'pointer';
 function subQty(){
+    let quantity = qtyInput.value;
     if(quantity > 1){
         document.getElementById('addQty').style.cursor = 'pointer';
         quantity--;
         console.log(quantity);
-        document.getElementById('orderQty').value = quantity;
-    }else if(quantity === 1){
+        qtyInput.value = quantity;
+    }else{
         document.getElementById('subQty').style.cursor = 'no-drop';
         console.log('You cannot order less than 1 item.')
-        return;
     }
 };
--- Project/Myapp/static/Myapp/JS/main.js
+++ Project/Myapp/static/Myapp/JS/main.js
@@ -1,47 +1,30 @@
-const addToCartBtn = document.querySelectorAll(".addToCart");
-addToCartBtn.forEach(btn =>{
-    btn.addEventListener('click', function(e){
-        e.preventDefault()
-        let productId = this.dataset.product;
-        let user = this.dataset.user;
-        console.log(user)
-        console.log(productId)
-        if(user === "AnonymousUser"){
-            window.location.href = `/Myapp/login/?next=${location.pathname}`;
-        } else {
-            url = "/Myapp/cart/"
-            fetch(url,{
-                method:'POST',
-                headers:{
-                'X-Requested-With': 'XMLHttpRequest',
-                'Content-Type':'application/json',
-                'X-CSRFToken': csrftoken
-                },
-                body: JSON.stringify({'productId':productId}),
-                credentials: 'same-origin'
-            })
-            .then(res => res.json())
-            .then((data)=>{
-                message = JSON.parse(data['message'])
-                const newP = document.createElement('p')
-                const successMsgId = 'successMsg-' + productId;
-                newP.id = successMsgId;
-                newP.className = 'successMsg';
-                newP.textContent = message;
-                document.body.appendChild(newP);
+/* Open the sidenav */
+function openNav() {
+    document.getElementById("mySidenav").style.cssText = `transition:0.5s; width:100%`
+}
 
-                // Hide the success message after 2 seconds
-                let lastSuccessMsg = null; // Keep track of the most recently added success message
-                setTimeout(function() {
-                    if (lastSuccessMsg) { // Hide the most recently added success message
-                        lastSuccessMsg.style.display = "none";
-                        lastSuccessMsg = null; // Reset lastSuccessMsg so it doesn't hide the wrong element later
-                    }
-                }, 2000);
-                lastSuccessMsg = newP; // Update lastSuccessMsg to the newly added success message
-            })
-        }
-    })
+/* Set the width of the side navigation to 0 */
+function closeNav(){
+    document.getElementById("mySidenav").style.cssText = `transition: 0.3s; width:0`
+} 
+
+const profileBtn = document.querySelector(".profile");
+profileBtn.addEventListener('click', ()=>{
+    profileTab();
 });
 
-
+const profileTab = (function(){
+    let clickStatus = false;
+    const profileCard = document.querySelector(".profile-card");
+    return function(){
+        if(clickStatus === false){
+            profileBtn.style.backgroundColor = "rgba(28, 172, 139, 0.8)";
+            profileCard.style.display = "flex";
+            clickStatus = true;     
+        }else{
+            profileBtn.style.backgroundColor = "white";
+            profileCard.style.display = "none";
+            clickStatus = false;
+        }
+    };
+})();
